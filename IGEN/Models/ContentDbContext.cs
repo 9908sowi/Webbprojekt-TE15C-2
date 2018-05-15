6c9c313911980ac63e232b97eebd80f18e840546
Elastisken <31006286+Elastisken@users.noreply.github.com>
2018-05-15 10:10:02 +0200

    Added a DbContext

---
+++ IGEN/Models/ContentDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace IGEN.Models
+{
+    public class ContentDbContext : DbContext
+    {
+        public DbSet<Game> Game { get; set; }
+        public DbSet<Article> Article { get; set; }
+        public DbSet<Gamedeveloper> Gamedeveloper { get; set; }
+    }
+}
